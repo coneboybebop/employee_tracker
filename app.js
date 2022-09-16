1e93cbebd17fd6cdcabac14d1734cd5b0390943e
Connor Allen <m.connor.allen@gmail.com>
2022-09-16 13:39:49 -0400

    add switch/case method for branching

--- app.js
+++ app.js
@@ -1,15 +1,82 @@
 const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const mysql = require('mysql2');
+const { getRandomValues } = require('crypto');
+const { allowedNodeEnvironmentFlags } = require('process');
+require('dotenv').config();
+
+//connect to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
 
 
 const initialPrompt = () => {
     inquirer.prompt([
         {
-            type: 'rawlist',
-            name: 'menu',
-            message: 'What would you like to do?',
-            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add an Employee', 'Update an Employee']
+          type: 'list',
+          name: 'menu',
+          message: 'What would you like to do?',
+          choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department','Add a Role', 'Add an Employee', 'Update an Employee']
         },
-    ])
+      ]).then(function ({ menu }) {
+
+    switch(menu) {
+      case 'View All Departments':
+        getDepartments();
+        break;
+
+      case 'View All Roles':
+        getRoles();
+        break;
+
+      case 'View All Employees':
+        getEmployees();
+        break;
+
+      case 'Add a Department':
+        addDepartment();
+        break;
+
+      case 'Add a Role':
+        addRole();
+        break;
+
+      case 'Add an Employee':
+        addEmployee();
+        break;
+
+      case 'Update an Employee':
+        updateEmployee();
+        break;
+      }
+    });
+
+       
 };
+
+const getDepartments = () => {
+    const sql = `SELECT id, name FROM department`;
+    connection.query(sql, (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      initialPrompt();
+    });
+
+};
+
+
 
 initialPrompt();
